70c428b4932bcfffcb2fe30689d4301fa8c2ee14
Andrew <akolisnyk@malkosua.com>
2017-03-17 21:02:36 +0100

    Add port env for node app and .vscode folder to gitignore

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ console.log(process.env);
 const express = require('express');
 
 // Constants
-const PORT = process.env.FOO || 8080;
+const PORT = process.env.PORT || 8080;
 const FOO = process.env.FOO || 'null';
 const BAR = process.env.BAR || 'null';
 
