c1abf1d6fccdcd2fdae2b27e3ef6bbaa501e6814
Andrew <akolisnyk@malkosua.com>
2017-03-21 11:25:34 +0100

    small code refactoring for bash script

--- docker-run.sh
+++ docker-run.sh
@@ -9,20 +9,20 @@ APP_NAME=$3
 IMAGE_APP_NAME=$4
 PORT=$5
 
-echo $ENV
+echo ${ENV}
 
 export APP_NAME_ENV="${APP_NAME}-${ENV}"
 export IMAGE_NAME=${IMAGE_APP_NAME}:${VERSION}
 
 echo "image name to use $IMAGE_NAME"
 
-docker rm -f $APP_NAME_ENV
+docker rm -f ${APP_NAME_ENV}
 
 docker run -e FOO="hello world!" \
 -e BAR="AWESOME" \
--e PORT=$PORT \
---name $APP_NAME_ENV \
--p $PORT:$PORT \
--d $IMAGE_NAME
+-e PORT=${PORT} \
+--name ${APP_NAME_ENV} \
+-p ${PORT}:${PORT} \
+-d ${IMAGE_NAME}
 
 echo "=============================="
